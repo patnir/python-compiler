b51f0c9e74fe0926d44baf9c9f0023190f2c3f8b
Rahul Patni <rahul20patni@gmail.com>
2021-06-19 14:10:53 -0400

    cleaner interface

--- compiler.py
+++ compiler.py
@@ -6,16 +6,16 @@ from typing import List
 @dataclasses.dataclass()
 class TokenType:
     re_expression: str
-    identifier: str
+    type: str
 
 
 TOKEN_TYPES = [
-    [r"\bdef\b", "def"],
-    [r"\bend\b", "end"],
-    [r"\b[a-zA-Z]+\b", "identifier"],
-    [r"\b[0-9]+\b", "integer"],
-    [r"\(", "oparen"],
-    [r"\)", "cparen"],
+    TokenType(r"\bdef\b", "def"),
+    TokenType(r"\bend\b", "end"),
+    TokenType(r"\b[a-zA-Z]+\b", "identifier"),
+    TokenType(r"\b[0-9]+\b", "integer"),
+    TokenType(r"\(", "oparen"),
+    TokenType(r"\)", "cparen"),
 ]
 
 NL = '\n'
@@ -42,18 +42,22 @@ class Tokenizer:
             result.extend(self._tokenize_one_token(token))
         return result
 
+    @staticmethod
+    def _get_search_result_length(search_result):
+        return search_result.span()[1] - search_result.span()[0]
+
     def _tokenize_one_token(self, token):
         for token_type in TOKEN_TYPES:
-            matching_token = rf"\A{token_type[0]}"
+            matching_token = rf"\A{token_type.re_expression}"
             search_result = re.search(matching_token, token)
             if search_result:
-                length = search_result.span()[1] - search_result.span()[0]
+                length = self._get_search_result_length(search_result)
                 if length != len(search_result.string):
                     remaining = search_result.string[length:]
-                    result = [Token(token_type=token_type[1], value=token[0:length])]
+                    result = [Token(token_type=token_type.type, value=token[0:length])]
                     result.extend(self._tokenize_one_token(remaining))
                     return result
-                return [Token(token_type=token_type[1], value=token)]
+                return [Token(token_type=token_type.type, value=token)]
         raise CouldNotMatchTokenException(f"could not match {token} to any tokens")
 
 
